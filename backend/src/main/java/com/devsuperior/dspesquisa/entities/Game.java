6b207a237d544227793cee07faafc39f8e60c630
kasilianaoliveira <kasilianaoliveira@gmail.com>
2020-09-16 10:25:18 -0300

    DTO, Service

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -13,12 +13,12 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.devsuperior.dspesquisa.entities.enums.Platform;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 @Table(name= "tb_game")
 public class Game implements Serializable{
-
-	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -26,11 +26,12 @@ public class Game implements Serializable{
 	private Long id;
 	private String title;
 	private Platform platform;
-	
+
+
 	@ManyToOne
 	@JoinColumn(name= "genre_id")
 	private Genre genre;
-	
+
 	@OneToMany(mappedBy = "game")
 	private List<Record> records = new ArrayList<>();
 
@@ -47,39 +48,48 @@ public class Game implements Serializable{
 	}
 
 	public Long getId() {
+
 		return id;
 	}
 
 	public void setId(Long id) {
+
 		this.id = id;
 	}
 
 	public String getTitle() {
+
 		return title;
 	}
 
 	public void setTitle(String title) {
+
 		this.title = title;
 	}
 
 	public Platform getPlatform() {
+
 		return platform;
 	}
 
 	public void setPlatform(Platform platform) {
+
 		this.platform = platform;
 	}
 
 	public Genre getGenre() {
+
 		return genre;
 	}
 
 	public void setGenre(Genre genre) {
+
 		this.genre = genre;
 	}
 	
 
 	public List<Record> getRecords() {
+
 		return records;
 	}
 
